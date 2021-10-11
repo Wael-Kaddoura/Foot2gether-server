1b58784ef86f42890b876fe992bbcdb8d7080980
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-11 17:06:42 +0300

    Added Match Controller

---
+++ controllers/MatchController.js
@@ -0,0 +1,8 @@
+const Validator = require("fastest-validator");
+const bcryptjs = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+
+const Models = require("../models");
+const Match = Models.User;
+
+module.exports = {};
