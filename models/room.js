73a0ec9b41dd20dfe25034e6ab92b32cb572e9f7
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-12 09:38:14 +0300

    Created Room Model

---
+++ models/room.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Room extends Model {
+    static associate(models) {}
+  }
+  Room.init(
+    {
+      match_id: {
+        type: Sequelize.INTEGER,
+      },
+      creator_id: {
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      current_participants_number: {
+        type: Sequelize.INTEGER,
+      },
+      max_capacity: {
+        type: Sequelize.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Room",
+    }
+  );
+  return Room;
+};
