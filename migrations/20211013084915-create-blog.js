67a755c73acf876785b692442246a47557d30cb6
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-13 11:54:22 +0300

    Adding Blogs Attributes

--- migrations/20211013084915-create-blog.js
+++ migrations/20211013084915-create-blog.js
@@ -1,27 +1,35 @@
-'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Blogs', {
+    await queryInterface.createTable("Blogs", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      body: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      author_id: {
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Blogs');
-  }
+    await queryInterface.dropTable("Blogs");
+  },
 };
--- models/blog.js
+++ models/blog.js
@@ -1,23 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Blog extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate(models) {}
+  }
+
+  Blog.init(
+    {
+      title: {
+        type: Sequelize.STRING,
+      },
+      body: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      author_id: {
+        type: Sequelize.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Blog",
     }
-  };
-  Blog.init({
-    title: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Blog',
-  });
+  );
   return Blog;
 };
