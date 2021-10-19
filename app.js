2aa7fff17a865273f92f145a54ad4c27a2cbf1f9
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-19 18:55:53 +0300

    Added CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv/config");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 8000;
 
@@ -11,8 +12,11 @@ const blogRoute = require("./routes/Blog");
 const fcmRoute = require("./routes/FCM");
 const imageRoute = require("./routes/Image");
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/images", express.static("images/uploads"));
 app.use("/logos", express.static("images/teams_logos"));
 
