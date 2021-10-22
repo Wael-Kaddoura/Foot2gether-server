8e251de80007a2ed41adf1901504ba964c0a4768
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-22 21:30:09 +0300

    Added BlogComment Migrations

---
+++ migrations/20211013084915-create-blog-comment.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Blogs_Comments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        type: Sequelize.STRING,
+      },
+      author_id: {
+        type: Sequelize.INTEGER,
+      },
+      blog_id: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Blogs");
+  },
+};
