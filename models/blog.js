2ac4c40bd68a69d76e44b12b0da4f639eff1d96c
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-22 21:30:42 +0300

    Added Relationship with BlogComment

--- models/blog.js
+++ models/blog.js
@@ -3,6 +3,11 @@ const { Sequelize, DataTypes, Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Blog extends Model {
     static associate(models) {
+      this.hasMany(models.BlogComment, {
+        as: "comments",
+        foreignKey: "blog_id",
+      });
+
       this.belongsTo(models.User, {
         as: "author",
         foreignKey: "author_id",
