7072d0ae7c43e4c606e64857aba9f2717fa45ccd
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-22 21:30:17 +0300

    Added BlogComment Seeders

--- seeders/20211011000001-blog-seeder.js
+++ seeders/20211011000001-blog-seeder.js
@@ -5,9 +5,53 @@ module.exports = {
         title: "United's Odds in the Premier League",
         body: `On Monday, Manchester United confirmed the signing of Cristiano Ronaldo in an official statement posted on social media. `,
 
-        image:
-          "http://localhost:8000/public/images/teams_logos/Manchester_United.png",
+        image: "http://localhost:8000/blogimg/blog2.jpg",
         author_id: "2",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Argentina to the Final!",
+        body: `Eveniet deleniti accusantium nulla natus nobis nam asperiores ipsa minima laudantium vero cumque cupiditate ipsum ratione dicta, expedita quae, officiis provident harum nisi! Esse eligendi ab molestias, quod nostrum hic saepe repudiandae non..`,
+
+        image: "http://localhost:8000/blogimg/blog3.jpg",
+        author_id: "1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Neymar to Balon Dor!",
+        body: `Eveniet deleniti accusantium nulla natus nobis nam asperiores ipsa minima laudantium vero cumque cupiditate ipsum ratione dicta, expedita quae, officiis provident harum nisi! Esse eligendi ab molestias, quod nostrum hic saepe repudiandae non..`,
+
+        image: "http://localhost:8000/blogimg/blog1.jpg",
+        author_id: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Neymar to Balon Dor!",
+        body: `Eveniet deleniti accusantium nulla natus nobis nam asperiores ipsa minima laudantium vero cumque cupiditate ipsum ratione dicta, expedita quae, officiis provident harum nisi! Esse eligendi ab molestias, quod nostrum hic saepe repudiandae non..`,
+
+        image: "http://localhost:8000/blogimg/blog1.jpg",
+        author_id: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "United's Odds in the Premier League",
+        body: `On Monday, Manchester United confirmed the signing of Cristiano Ronaldo in an official statement posted on social media. `,
+
+        image: "http://localhost:8000/blogimg/blog2.jpg",
+        author_id: "2",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Argentina to the Final!",
+        body: `Eveniet deleniti accusantium nulla natus nobis nam asperiores ipsa minima laudantium vero cumque cupiditate ipsum ratione dicta, expedita quae, officiis provident harum nisi! Esse eligendi ab molestias, quod nostrum hic saepe repudiandae non..`,
+
+        image: "http://localhost:8000/blogimg/blog3.jpg",
+        author_id: "1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
