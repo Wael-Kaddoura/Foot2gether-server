8d667b16be875c4ac80d52a944a3ef5741560d30
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-25 20:06:53 +0300

    Implemented Send Notifications on Room Creation

--- controllers/FCMController.js
+++ controllers/FCMController.js
@@ -3,13 +3,19 @@ const SERVER_KEY =
   "AAAAmDQh_nE:APA91bFcJnYtohVwbGoQv9RSiEZGvn6RDDsIP6bwMx9IYb_b1xXZGteaSZzOT84mVycaU3zVoW02EbfJHE4gu15h-38Wtylb8U5-_yeSL0zJoJpLO7MMISRiDTPf9UJ-nojA8ml4KM88";
 
 async function sendNotification(req, res, next) {
+  let { creator_username, team1, team2 } = req.notificationInfo;
+
+  let title = "New Room Created!";
+  let body = `${creator_username} created a room to watch ${team1} vs ${team2}!`;
+  let regTokens = req.followersTokens;
+
   try {
     let fcm = new FCM(SERVER_KEY);
     let message = {
-      to: "fr0z-B2m8M1Xn72kQc97zv:APA91bEUjSubqXn9i6NnjkZwvUvslzyD8FM6EiSUVmJcSA_oqJOcoo_o371JHrQQA4nSL0EqJoxu2P9tm3LiUzeqHXVw-A4h68fYg3jJ0cddS5gCk5EvxXd9vf8TjC1heKubBYnbt-sp",
+      registration_ids: regTokens,
       notification: {
-        title: req.body.title,
-        body: req.body.body,
+        title,
+        body,
         // click_action: "FCM_PLUGIN_ACTIVITY",
         // icon: "fcm_push_icon",
       },
--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -3,7 +3,8 @@ const Op = Sequelize.Op;
 const date = require("date-and-time");
 const Validator = require("fastest-validator");
 
-const { Room, Match } = require("../models");
+const { Room, Match, User } = require("../models");
+const FCMController = require("./FCMController");
 
 const current_date_time = new Date();
 const current_date = date.format(current_date_time, "YYYY-MM-DD");
@@ -103,7 +104,7 @@ async function getMyRooms(req, res) {
   res.send(response);
 }
 
-async function createRoom(req, res) {
+async function createRoom(req, res, next) {
   const v = new Validator();
   const schema = {
     match_id: { type: "string", optional: false },
@@ -122,19 +123,47 @@ async function createRoom(req, res) {
   const { match_id, name } = req.body;
   const creator_id = req.userData.user_id;
 
-  const room = {
+  const new_room = {
     match_id,
     creator_id,
     name,
-    current_participants_number: 0,
   };
 
-  try {
-    const response = await Room.create(room);
-    res.status(201).json({
-      message: "Room created successfully!",
-      room: response,
+  const match_info = await Match.findOne({
+    where: { id: match_id },
+    include: { all: true },
+  });
+
+  const creator_info = await User.findOne({
+    where: { id: creator_id },
+    include: "follower",
+  });
+
+  const followers_list = creator_info.follower;
+  let followers_tokens = [];
+
+  //getting notification token of users
+  for (const follower of followers_list) {
+    const follower_token = await User.findOne({
+      where: { id: follower.user_id },
+      attributes: ["notification_token"],
     });
+
+    followers_tokens.push(follower_token.notification_token);
+  }
+
+  const notification_info = {
+    creator_username: creator_info.username,
+    team1: match_info.team1.name,
+    team2: match_info.team2.name,
+  };
+
+  req.notificationInfo = notification_info;
+  req.followersTokens = followers_tokens;
+
+  try {
+    const response = await Room.create(new_room);
+    FCMController.sendNotification(req, res, next);
   } catch (error) {
     res.status(500).json({
       message: "Something went wrong!",
