0269ed5851bdafa5a8f4e3d015e2afbb05bba870
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-11-02 11:35:44 +0200

    Modified Blog Image Uploader 2

--- middleware/blog-image-uploader.js
+++ middleware/blog-image-uploader.js
@@ -10,19 +10,8 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(new Error("Unsupported file format!"), false);
-  }
-};
-
 const upload = multer({
   storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 10,
-  },
 });
 
 module.exports = {
